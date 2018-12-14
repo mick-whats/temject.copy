c962557fdfa9c55e08b9faf7027c8a95c9131054
whats-mick <9634573+mick-whats@users.noreply.github.com>
2018-12-15 08:56:25 +0900

    fix add globby

--- src/__tests__/expressionFiles.test.js
+++ src/__tests__/expressionFiles.test.js
@@ -1,8 +1,37 @@
-const temjectCopy = require('../temjectCopy')
+const expressionFiles = require('../expressionFiles')
 const os = require('os')
 const fs = require('fs')
-const writeFileAtomic = require('write-file-atomic')
 const path = require('path')
-const srcPath = path.join(os.tmpdir(), 'temjectCopySrc.txt')
-const distDir = path.join(os.tmpdir(), 'testDir')
-const distPath = path.join(distDir, 'temjectCopyDist.txt')
+const globby = require('globby')
+
+test('expressionFiles', async () => {
+  const paths = await globby('./src/__tests__/testFiles', {
+    gitignore: true,
+    dot: true
+  })
+  const res = await expressionFiles(paths)
+  expect(res).toHaveLength(4)
+  expect(res).toContain('readme')
+  expect(res).toContain('ig')
+  expect(res).toContain('home')
+  expect(res).toContain('name')
+})
+
+test('expressionFiles glob', async () => {
+  const res = await expressionFiles('./src/__tests__/testFiles/')
+  expect(res).toHaveLength(4)
+  expect(res).toContain('readme')
+  expect(res).toContain('ig')
+  expect(res).toContain('home')
+  expect(res).toContain('name')
+})
+test('expressionFiles glob', async () => {
+  const res = await expressionFiles([
+    './src/__tests__/testFiles/',
+    '!**/ignore.txt'
+  ])
+  expect(res).toHaveLength(3)
+  expect(res).toContain('readme')
+  expect(res).toContain('home')
+  expect(res).toContain('name')
+})
--- src/expressionFiles.js
+++ src/expressionFiles.js
@@ -1,17 +1,30 @@
-const fs = require('fs')
 const path = require('path')
-const { temject } = require('temject')
-const writeFileAtomic = require('write-file-atomic')
-const makeDir = require('make-dir')
+const readFile = require('./readFile')
 const globby = require('globby')
+const { expressions } = require('temject')
+
 /**
- * temjectCopy
+ * expressionFiles
+ * fileに書き込まれているexpressionsを抽出する
  *
- * @param {string} srcPath - src file path
- * @param {string} distPath - dist file path
- * @param {Object} injections - inject key and value
- * @return {Promise}
+ * @param {Array} glob - paths or glob
+ * @param {Object} [opts={}]
+ * @return {Promise<Array>}
+ * @example
+ * const opts = {ignoreFile: ['ignore.txt']}
+ * const res = await expressionFiles(paths, opts)
+ * // -> [ 'name', 'home', 'readme' ]
  */
-module.exports = function expressionFiles (dir, opts) {
-  return new Promise((resolve, reject) => {})
+module.exports = function expressionFiles (glob, opts = {}) {
+  return new Promise(async (resolve, reject) => {
+    console.log('glob: ', glob)
+    const paths = await globby(glob, { dot: true })
+    console.log('paths: ', paths)
+    Promise.all(paths.map(p => readFile(p)))
+      .then(items => {
+        const res = expressions(items.join(''))
+        return resolve(res)
+      })
+      .catch(reject)
+  })
 }
